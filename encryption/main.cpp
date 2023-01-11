e6b86c41ee4fe832a40da6210b9f1ef8c831e7dc
skirt-owner <skirtsfield@gmail.com>
2023-01-11 20:51:11 +0300

    first commit

---
+++ encryption/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  encryption
+//
+//  Created by skirt owner on 11.01.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
